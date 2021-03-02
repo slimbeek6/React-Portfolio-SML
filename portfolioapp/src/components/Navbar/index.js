a8920422476d8d3677c0545f0b99631b5fe896bb
Shaun Limbeek <soccercomet@yahoo.com>
2021-03-01 18:28:42 -0800

    create the pages framework, APP framework, and Navbar

---
+++ portfolioapp/src/components/Navbar/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+
+  return (
+    <nav class="navbar">
+        <div class="navbar-brand">
+            <Link to={"/"}>
+                <h1 class="title" >Shaun Limbeek</h1>
+            </Link>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarRightSide">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarRightSide" class="navbar-menu">
+            <div class="navbar-end">
+                <Link to={"/about"} class="navbar-item menuItem" >About</Link>
+                <Link to={"/portfolio"} class="navbar-item menuItem">Portfolio</Link>
+                <Link to={"/contact"} class="navbar-item menuItem" >Contact</Link>
+            </div>
+        </div>
+    </nav>
+  );
+}
+
+export default Nav;
