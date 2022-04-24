d3a604dc4e05f2f1b0c04f502d0d01ea929be787
Shaun Limbeek <slimbeek@shauns-mbp.lan>
2022-04-24 13:01:54 -0700

    Update the Contact page with new formatting

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,34 +4,34 @@ const Contact = () => {
     return (
         <div>
             <br />
-        <div class="columns">
-            <div class="card column is-full" id="contactPane">
+        <div class="row">
+            <div class="card col-12" id="contactPane">
                     <h3 class="title">Getting in Touch</h3>
                     
                     <br />
                     
                     {/* Column Headers */}
-                    <div class="columns">
-                        <h4 id="emailSect" class="column is-one-third subtitle" style={{textAlign: "center"}}><strong>Email: </strong></h4>
-                        <h4 id="emailSect" class="column is-one-third subtitle" style={{textAlign: "center"}}><strong>LinkedIn: </strong></h4>
-                        <h4 id="emailSect" class="column is-one-third subtitle" style={{textAlign: "center"}}><strong>Github: </strong></h4>
+                    <div class="row">
+                        <h4 id="emailSect" class="col-4 subtitle" style={{textAlign: "center"}}><strong>Email: </strong></h4>
+                        <h4 id="emailSect" class="col-4 subtitle" style={{textAlign: "center"}}><strong>LinkedIn: </strong></h4>
+                        <h4 id="emailSect" class="col-4 subtitle" style={{textAlign: "center"}}><strong>Github: </strong></h4>
                     </div>
 
                     {/* Links to contact information including images */}
-                    <div class="columns">
-                        <article class="column is-one-third">
+                    <div class="row">
+                        <article class="col-4 card">
                             <a href="mailto:shaun.limbeek@gmail.com">
                                 <img src="../assets/send-email.png" alt="Email being sent" id="sendemail" />
                             </a>
                         </article>
 
-                        <article class="column is-one-third">
+                        <article class="col-4 card">
                             <a href="https://www.linkedin.com/in/shaun-limbeek/">
                                 <img src="../assets/LI-logo.png" alt="LinkedIn logo" id="LI-logo" style={{maxWidth: "250px"}}/>
                             </a>
                         </article>
 
-                        <article class="column is-one-third">
+                        <article class="col-4 card">
                             <a href="https://github.com/slimbeek6/">
                                 <img src="../assets/GitHub-logo.png" alt="Github logo" id="Github-logo" style={{maxWidth: "250px"}}/>
                             </a>
