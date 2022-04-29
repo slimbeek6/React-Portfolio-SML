b45048f5cb9843092a4b1fc4f0835c0a0987941a
Shaun Limbeek <slimbeek@Shauns-MacBook-Pro.local>
2022-04-29 17:42:11 -0400

    Added link to Hackerrank

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,28 +12,35 @@ const Contact = () => {
                     
                     {/* Column Headers */}
                     <div class="row">
-                        <h4 id="emailSect" class="col-4 subtitle" style={{textAlign: "center"}}><strong>Email: </strong></h4>
-                        <h4 id="emailSect" class="col-4 subtitle" style={{textAlign: "center"}}><strong>LinkedIn: </strong></h4>
-                        <h4 id="emailSect" class="col-4 subtitle" style={{textAlign: "center"}}><strong>Github: </strong></h4>
+                        <h4  class="col-3 subtitle" style={{textAlign: "center"}}><strong>Email: </strong></h4>
+                        <h4  class="col-3 subtitle" style={{textAlign: "center"}}><strong>LinkedIn: </strong></h4>
+                        <h4  class="col-3 subtitle" style={{textAlign: "center"}}><strong>Github: </strong></h4>
+                        <h4  class="col-3 subtitle" style={{textAlign: "center"}}><strong>HackerRank: </strong></h4>
                     </div>
 
                     {/* Links to contact information including images */}
                     <div class="row" >
-                        <div class="col-4" style={{background: "none"}}>
+                        <div class="col-3" style={{background: "none"}}>
                             <a href="mailto:shaun.limbeek@gmail.com">
                                 <img src="../assets/send-email.png" alt="Email being sent" class="contactImg" />
                             </a>
                         </div>
 
-                        <div class="col-4" style={{background: "none"}}>
+                        <div class="col-3" style={{background: "none"}}>
                             <a href="https://www.linkedin.com/in/shaun-limbeek/">
                                 <img src="../assets/LI-logo.png" alt="LinkedIn logo" class="contactImg" />
                             </a>
                         </div>
 
-                        <div class="col-4" style={{background: "none"}}>
+                        <div class="col-3" style={{background: "none"}}>
                             <a href="https://github.com/slimbeek6/">
                                 <img src="../assets/GitHub-logo.png" alt="Github logo" class="contactImg" />
+                            </a>
+                        </div>
+
+                        <div class="col-3" style={{background: "none"}}>
+                            <a href="https://www.hackerrank.com/shaun_limbeek">
+                                <img src="../assets/HR-logo.jpeg" alt="HackerRank logo" class="contactImg" />
                             </a>
                         </div>
                     </div>
